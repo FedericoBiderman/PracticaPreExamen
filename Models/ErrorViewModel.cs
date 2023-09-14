dba1b2dc11b1ad26433c5f1d4b577b9476b61659
FEDERICO EITAN BIDERMAN <48038607@ort>
2023-09-14 13:28:30 -0300

    inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PracticaPreExamen.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
